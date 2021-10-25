3a3687dae2c667123e907707cc314b4a07644f1d
gxdxx <nkd0310@naver.com>
2021-10-26 01:22:10 +0900

    feat: 공공데이터포털 도서목록 API 연결

--- routes/search.js
+++ routes/search.js
@@ -2,18 +2,69 @@ const express = require("express");
 const { Book, User } = require("../models");
 const router = express.Router();
 
-router.get("/book/", async (req, res, next) => {
+const request = require("request");
+
+router.get("/library/", async (req, res, next) => {
+  const url =
+    "http://apis.data.go.kr/6260000/BookNewListService/getBookNewList";
+  let queryParams =
+    "?" +
+    encodeURIComponent("serviceKey") +
+    "=Gc%2FEM8UVhS6aGwllwxru0f09DP%2Bu%2FNUYy74JAjUFf7FC6q1%2Bf0vz6C5Cz7uxuOtw7X8HrhsWE98kcd%2F9PfX9sw%3D%3D"; /* Service Key*/
+  queryParams +=
+    "&" +
+    encodeURIComponent("numOfRows") +
+    "=" +
+    encodeURIComponent("10"); /* */
+  queryParams +=
+    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("resultType") +
+    "=" +
+    encodeURIComponent("json"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("title_info") +
+    "=" +
+    encodeURIComponent("나는 간디야"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("shelf_date") +
+    "=" +
+    encodeURIComponent("2021/09/14"); /* */
+  queryParams +=
+    "&" +
+    encodeURIComponent("author") +
+    "=" +
+    encodeURIComponent(
+      "브래드 멜처 글 ; 크리스토퍼 엘리오풀로스 그림 ; 마술연필 옮김"
+    ); /* */
+
+  request(
+    {
+      url: url + queryParams,
+      method: "GET",
+    },
+    function (error, response, body) {
+      console.log("Status", response.statusCode);
+      console.log("Headers", JSON.stringify(response.headers));
+      console.log("Reponse received", body);
+    }
+  );
+
   try {
     const books = await Book.findAll({
       where: {
-        book: req.query.search_book,
+        title: req.query.search_book,
       },
       order: [["createdAt", "DESC"]],
     });
-    res.render("index", {
-      title: "YU도서",
-      books: books,
-    });
+    // res.render("index", {
+    //   title: "YU도서",
+    //   books: books,
+    // });
+    res.redirect("/#board");
   } catch (err) {
     console.error(err);
     next(err);
