d0da41a71f15411de3b3d1be2135eefd6a6ad868
gxdxx <nkd0310@naver.com>
2021-10-28 23:01:45 +0900

    feat: API json값 추출

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,7 @@ const { Book, User } = require("../models");
 const router = express.Router();
 
 const request = require("request");
+const parse = require("json-parse");
 
 router.get("/library/", async (req, res, next) => {
   const url =
@@ -12,10 +13,7 @@ router.get("/library/", async (req, res, next) => {
     encodeURIComponent("serviceKey") +
     "=Gc%2FEM8UVhS6aGwllwxru0f09DP%2Bu%2FNUYy74JAjUFf7FC6q1%2Bf0vz6C5Cz7uxuOtw7X8HrhsWE98kcd%2F9PfX9sw%3D%3D"; /* Service Key*/
   queryParams +=
-    "&" +
-    encodeURIComponent("numOfRows") +
-    "=" +
-    encodeURIComponent("10"); /* */
+    "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("2"); /* */
   queryParams +=
     "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
   queryParams +=
@@ -27,19 +25,19 @@ router.get("/library/", async (req, res, next) => {
     "&" +
     encodeURIComponent("title_info") +
     "=" +
-    encodeURIComponent("나는 간디야"); /* */
-  queryParams +=
-    "&" +
-    encodeURIComponent("shelf_date") +
-    "=" +
-    encodeURIComponent("2021/09/14"); /* */
-  queryParams +=
-    "&" +
-    encodeURIComponent("author") +
-    "=" +
-    encodeURIComponent(
-      "브래드 멜처 글 ; 크리스토퍼 엘리오풀로스 그림 ; 마술연필 옮김"
-    ); /* */
+    encodeURIComponent(req.query.search_book); /* */
+  // queryParams +=
+  //   "&" +
+  //   encodeURIComponent("shelf_date") +
+  //   "=" +
+  //   encodeURIComponent("2021/09/14"); /* */
+  // queryParams +=
+  //   "&" +
+  //   encodeURIComponent("author") +
+  //   "=" +
+  //   encodeURIComponent(
+  //     "브래드 멜처 글 ; 크리스토퍼 엘리오풀로스 그림 ; 마술연필 옮김"
+  //   ); /* */
 
   request(
     {
@@ -50,25 +48,37 @@ router.get("/library/", async (req, res, next) => {
       console.log("Status", response.statusCode);
       console.log("Headers", JSON.stringify(response.headers));
       console.log("Reponse received", body);
+      console.log("@#!!#");
+      const obj = JSON.parse(body);
+      books = obj.getBookNewList.item;
+      console.log(books[0]);
+      console.log(obj.getBookNewList.item[0].publisher);
+      console.log(obj.getBookNewList.item[1].publisher);
+      console.log("@!#");
+
+      res.render("searchResult", {
+        title: "YU도서",
+        books: books,
+      });
     }
   );
 
-  try {
-    const books = await Book.findAll({
-      where: {
-        title: req.query.search_book,
-      },
-      order: [["createdAt", "DESC"]],
-    });
-    // res.render("index", {
-    //   title: "YU도서",
-    //   books: books,
-    // });
-    res.redirect("/#board");
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+  // try {
+  //   const books = await Book.findAll({
+  //     where: {
+  //       title: req.query.search_book,
+  //     },
+  //     order: [["createdAt", "DESC"]],
+  //   });
+  //   // res.render("index", {
+  //   //   title: "YU도서",
+  //   //   books: books,
+  //   // });
+  //   res.redirect("/#board");
+  // } catch (err) {
+  //   console.error(err);
+  //   next(err);
+  // }
 });
 
 module.exports = router;
