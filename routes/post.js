9640048a962dcbf25243f5010608cf8a0a4e7b06
gxdxx <nkd0310@naver.com>
2021-11-16 14:57:32 +0900

    feat: 게시글, 댓글 삭제 기능 구현

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,6 @@ const { isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
-// 게시글 업로드
 router.post("/", isLoggedIn, async (req, res, next) => {
   try {
     await Post.create({
@@ -13,7 +12,22 @@ router.post("/", isLoggedIn, async (req, res, next) => {
       title: req.body.writeTitle,
       content: req.body.writeContent,
       UserId: req.user.id,
+      snsId: req.user.snsId,
     });
+    res.redirect("/board/page/1");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post("/delete/:id", isLoggedIn, async (req, res, next) => {
+  try {
+    await Post.update(
+      { deleted: true },
+      { where: { UserId: req.user.id, id: req.params.id } }
+    );
+
     res.redirect("/board/page/1");
   } catch (error) {
     console.error(error);
@@ -24,16 +38,28 @@ router.post("/", isLoggedIn, async (req, res, next) => {
 router.post("/comment/:id", isLoggedIn, async (req, res, next) => {
   try {
     await Comment.create({
+      UserId: req.user.id,
+      snsId: req.user.snsId,
       PostId: req.params.id,
       email: req.user.email,
       nick: req.user.nick,
       content: req.body.writeContent,
     });
-    console.log("@#!!#!");
-    console.log(req.body.writeContent);
-    console.log(req.body.anonymousCheck);
-    console.log(req.params.id);
     res.redirect("/board/" + req.params.id);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post("/comment/delete/:id", isLoggedIn, async (req, res, next) => {
+  try {
+    const postId = await Comment.findOne({ where: { id: req.params.id } });
+
+    await Comment.destroy({
+      where: { UserId: req.user.id, id: req.params.id },
+    });
+    res.redirect("/board/" + postId.PostId);
   } catch (error) {
     console.error(error);
     next(error);
